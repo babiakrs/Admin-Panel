cfa4353cb615ed4d47b4629a37c168a5450a622e
Alfinity <its.alfinity@gmail.com>
2020-09-04 12:43:55 +0300

    Header html

--- source/components/Header/index.jsx
+++ source/components/Header/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 
+import { Icon } from 'Components/Icon';
+
 import './styles.sass';
 
 function Header() {
   return (
     <header>
-
+      <div className='sb__toggler'>
+        <Icon className='sb__toggler-icon' type='menu'/>
+      </div>
+      <navbar></navbar>
+      <div className='h__account'></div>
     </header>
   );
 }
