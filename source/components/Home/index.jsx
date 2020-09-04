94f84bf7b92c25b51061f8ebc34e2e51427048d4
Alfinity <its.alfinity@gmail.com>
2020-09-04 14:39:44 +0300

    Added home page

---
+++ source/components/Home/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.sass';
+
+function Home() {
+  return (
+    <div className='home'>
+      Тестовое задание :)
+    </div>
+  );
+}
+
+export { Home };
