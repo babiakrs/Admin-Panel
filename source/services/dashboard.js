7eb3d914da6edc0772d8986dbaa6856fef8f3e78
Alfinity <its.alfinity@gmail.com>
2020-09-07 14:09:55 +0300

    Change api protocol to https

--- source/services/dashboard.js
+++ source/services/dashboard.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 class DashboardService {
-  _apiBase = 'http://my-json-server.typicode.com/Almost-Infinity/placeholder-db';
+  _apiBase = 'https://my-json-server.typicode.com/Almost-Infinity/placeholder-db';
 
   getCurrencies() {
     return axios.get(`${this._apiBase}/currencies`);
