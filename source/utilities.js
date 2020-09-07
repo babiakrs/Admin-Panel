e8212be37c59639cd6dfa62453dfbe9d7f9cf5ef
Alfinity <its.alfinity@gmail.com>
2020-09-07 14:51:00 +0300

    Add class functions

--- source/utilities.js
+++ source/utilities.js
@@ -1,3 +1,19 @@
 export function cn(...cns) {
   return cns.reduce((acc, curr) => `${acc} ${curr}`).trim();
 }
+
+export function hasClass(el, clazz) {
+  return new RegExp(`(^| )(${clazz})($| )`).test(el.className);
+}
+
+export function addClass(el, clazz) {
+  return (!hasClass(el, clazz)) ? (el.className += ` ${clazz}`).trim() : false;
+}
+
+export function removeClass(el, clazz) {
+  return (hasClass(el, clazz)) ? el.className = el.className.split(' ').filter((c) => c !== clazz).join(' ') : false;
+}
+
+export function toggleClass(el, clazz) {
+  return hasClass(el, clazz) ? removeClass(el, clazz) : addClass(el, clazz);
+}
