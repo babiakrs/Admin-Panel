4cc860b91e38f97bc196d278f6e4415c9d63abb3
Alfinity <its.alfinity@gmail.com>
2020-11-11 12:00:14 +0200

    Link to github

--- source/components/Home/index.jsx
+++ source/components/Home/index.jsx
@@ -1,13 +1,18 @@
 import React, { useEffect } from 'react';
 
+import { Icon } from 'Components/Icon';
+
 import './styles.sass';
 
 function Home() {
   useEffect(() => { document.title = 'Admin Panel • Home'; });
 
   return (
-    <div className='home'>
-      Тестовое задание :)
+    <div className='home__wrapper'>
+      <a href='https://github.com/Almost-Infinity/Admin-Panel' target='_blank' rel='noreferrer' className='home__link'>
+        <Icon type='github' className='home__icon'/>
+        Source on github
+      </a>
     </div>
   );
 }
