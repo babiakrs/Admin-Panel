b1bfc85c4c99bd312d58cba150d96e5206545c62
Alfinity <its.alfinity@gmail.com>
2020-12-05 16:59:15 +0200

    Add user's section

--- source/components/Header/index.jsx
+++ source/components/Header/index.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 import { func, string } from 'prop-types';
 
 import { Icon } from 'Components/Icon';
+import { Picture } from 'Components/Picture';
+
+import webpPhAvatar from 'Images/ph-avatar.webp';
+import jpegPhAvatar from 'Images/ph-avatar.jpg';
 
 import './styles.sass';
 
@@ -10,6 +14,10 @@ function Header({ toggleSidebar, className }) {
     <header className={className}>
       <div className='sb__toggler' onClick={toggleSidebar}>
         <Icon className='sb__toggler-icon' type='menu'/>
+      </div>
+      <div className='user__wrapper'>
+        <Picture className='user__avatar' webpURL={webpPhAvatar} fallbackURL={jpegPhAvatar} fallbackText='Avatar'/>
+        <span className='user__name'>John Doe</span>
       </div>
     </header>
   );
